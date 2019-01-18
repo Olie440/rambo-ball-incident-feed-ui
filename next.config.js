2fb7d2e5ac09d7816ef14d7ef93c2744f6af9e56
Oliver Rushworth <oliver@oliverushworth.uk>
2019-01-18 14:24:05 +0000

    [Closes #3] - Add linting

--- next.config.js
+++ next.config.js
@@ -1,2 +1,20 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({ cssModules: true });
+module.exports = withSass({ 
+    cssModules: true,
+    webpack: (config, { dev }) => {
+        const eslintRule = {
+            test: /\.(js|jsx)$/,
+            enforce: 'pre',
+            exclude: /node_modules/,
+            loader: 'eslint-loader',
+            options: {
+                // Emit errors as warnings for dev to not break webpack build.
+                // Eslint errors are shown in console for dev, yay :-)
+                emitWarning: dev,
+            },
+        };
+
+        config.module.rules.push(eslintRule);
+        return config;
+    },
+});
