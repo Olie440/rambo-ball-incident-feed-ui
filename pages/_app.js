99ba56982b5051ebd3c4a160527b33587a7a994d
Oliver Rushworth <oliver@oliverushworth.uk>
2019-01-21 10:02:11 +0000

    Fix cross origin prop error

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,12 @@ class MyApp extends App {
     render() {
         const { Component, pageProps, reduxStore } = this.props
         return (
-            <Container className="aaa">
+            <Container>
                 <Head>
-                    <script src="https://use.fontawesome.com/releases/v5.6.3/js/all.js" integrity="sha384-EIHISlAOj4zgYieurP0SdoiBYfGJKkgWedPHH4jCzpCXLmzVsw1ouK59MuUtP4a1" crossorigin="anonymous"></script>
+                    <script
+                        src="https://use.fontawesome.com/releases/v5.6.3/js/all.js"
+                        integrity="sha384-EIHISlAOj4zgYieurP0SdoiBYfGJKkgWedPHH4jCzpCXLmzVsw1ouK59MuUtP4a1"
+                        crossOrigin="anonymous" />
                     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
                     <link rel="stylesheet" href="/static/styles/reset.css" />
                     <link rel="stylesheet" href="/static/styles/global.css" />
